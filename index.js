38852360cc1555975f2450b740c0b47d3a40d19a
marina-nagaoka1 <64458327+marina-nagaoka1@users.noreply.github.com>
2021-12-11 00:43:35 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var img;
+
+function sampleimg1(){
+img = document.getElementById("image_file");
+img.src = "image/sample1.png";
+}
+
+function sampleimg2(){
+img = document.getElementById("image_file");
+img.src = "image/sample2.png";
+}
+
+function sampleimg3(){
+img = document.getElementById("image_file");
+img.src = "image/sample3.png";
+}
